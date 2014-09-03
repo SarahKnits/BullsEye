28047c13ce94f402dfaecc0d78e5df062d23f6ea
Sarah Gilkinson <sarahknits810@gmail.com>
2014-09-02 19:37:20 -0700

    Completed tutorial through page 60

--- BullsEye/SGViewController.h
+++ BullsEye/SGViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface SGViewController : UIViewController
+@property (nonatomic, weak) IBOutlet UISlider *slider;
 
 - (IBAction)showAlert;
 - (IBAction)sliderMoved:(UISlider *)slider;
