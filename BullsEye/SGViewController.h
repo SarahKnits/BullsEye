f3c4570f1c7ca295028d3702ffb8b01e03cf414c
Sarah Gilkinson <sarahknits810@gmail.com>
2014-09-02 19:53:44 -0700

    Completed tutorial through page 82

--- BullsEye/SGViewController.h
+++ BullsEye/SGViewController.h
@@ -11,6 +11,8 @@
 @interface SGViewController : UIViewController
 @property (nonatomic, weak) IBOutlet UISlider *slider;
 @property (nonatomic, weak) IBOutlet UILabel *targetLabel;
+@property (nonatomic, weak) IBOutlet UILabel *scoreLabel;
+@property (nonatomic, weak) IBOutlet UILabel *roundLabel;
 
 - (IBAction)showAlert;
 - (IBAction)sliderMoved:(UISlider *)slider;
--- BullsEye/SGViewController.m
+++ BullsEye/SGViewController.m
@@ -16,6 +16,8 @@
 {
     int _currentValue;
     int _targetValue;
+    int _score;
+    int _round;
 }
 
 - (void)viewDidLoad
@@ -33,14 +35,21 @@
 
 - (IBAction)showAlert
 {
+    int difference;
+    difference = _targetValue - _currentValue;
+    if(_currentValue > _targetValue) {
+        difference = -difference;
+    }
+    int points = 100 - difference;
+    _score += points;
     NSString *message = [NSString stringWithFormat:
-                         @"The value of the slider is: %d\nThe target value is: %d", _currentValue, _targetValue];
+                         @"You scored %d points", points];
     
     UIAlertView *alertView = [[UIAlertView alloc]
-        initWithTitle:@"Hello, World"
+        initWithTitle:@"Bull's Eye!"
         message:message
         delegate:nil
-        cancelButtonTitle:@"Awesome"
+        cancelButtonTitle:@"OK"
         otherButtonTitles:nil];
     [alertView show];
     [self startNewRound];
@@ -54,6 +63,7 @@
 
 - (void)startNewRound
 {
+    _round += 1;
     _targetValue = 1 + arc4random_uniform(100);
     _currentValue = 50;
     self.slider.value = _currentValue;
@@ -62,5 +72,7 @@
 - (void)updateLabels
 {
     self.targetLabel.text = [NSString stringWithFormat:@"%d", _targetValue];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%d", _score];
+    self.roundLabel.text = [NSString stringWithFormat:@"%d", _round];
 }
 @end
