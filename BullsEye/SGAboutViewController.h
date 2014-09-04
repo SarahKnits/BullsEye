a90d39144c23401925bf74ce741ef231f3a3ad8c
Sarah Gilkinson <sarahknits810@gmail.com>
2014-09-04 09:04:25 -0700

    Completed tutorial through page 100, section Adding the About Screen

---
+++ BullsEye/SGAboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  SGAboutViewController.h
+//  BullsEye
+//
+//  Created by Sarah Gilkinson on 9/4/14.
+//  Copyright (c) 2014 SarahGilkinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGAboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
